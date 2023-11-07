d7ccb145fcd4a0460906a71fd713781ad85684b5
Miguel OTERO <motero@student.42.fr>
2023-11-07 17:19:08 +0100

    dom added to inception as wella s a button

--- srcs/requirements/wordpress/tools/wpscript.sh
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -17,9 +17,6 @@ if [ ! -f "wp-config.php" ]; then
     chown -R root:root /var/www/html
 fi
 
-# Check if WordPress is already installed
-echo "Checking if WordPress is already installed..."
-#if ! $(wp core is-installed --allow-root); then
     # Setting up WordPress
     echo "Installing WordPress..."
     wp core install --allow-root --url="$SITE_URL" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL" --path='/var/www/html'
@@ -27,7 +24,48 @@ echo "Checking if WordPress is already installed..."
     # Add another user
     echo "Adding another user..."
     wp user create $SECOND_USER_NAME $SECOND_USER_EMAIL  --allow-root   --role=author --user_pass=$SECOND_USER_PASS --path='/var/www/html'
-#fi
+
+#CODE INTEGRATION FROM HERE --------------------------------------------------------------------------------
+# Wait for the database to be fully ready
+echo "Waiting for database to be fully ready..."
+MAX_TRIES=10
+TRIES=0
+while ! wp db check --allow-root --path='/var/www/html' > /dev/null 2>&1; do
+    sleep 5
+    TRIES=$((TRIES+1))
+    if [ "$TRIES" -ge "$MAX_TRIES" ]; then
+        echo "Database did not become ready in time."
+        exit 1
+    fi
+done
+
+echo "Checking if a static front page is set..."
+HOMEPAGE_ID=$(wp option get page_on_front --allow-root --path='/var/www/html')
+
+if [ "$HOMEPAGE_ID" -eq 0 ]; then
+    echo "No static front page set. Creating a new homepage..."
+
+    # Create a new homepage
+    HOMEPAGE_ID=$(wp post create --post_type=page --post_title='Home' --post_content='Welcome to my website!' --post_status=publish --allow-root --path='/var/www/html' --porcelain)
+    echo "New homepage created with ID: $HOMEPAGE_ID"
+
+    # Set the newly created page as the front page
+    wp option update show_on_front 'page' --allow-root --path='/var/www/html'
+    wp option update page_on_front "$HOMEPAGE_ID" --allow-root --path='/var/www/html'
+else
+    echo "Static front page already set with ID: $HOMEPAGE_ID"
+fi
+
+# Replace the placeholder with the iframe
+echo "Adding iframe and button to the homepage content..."
+IFRAME_CODE='<iframe src="https://motero.42.fr/doom" width="600" height="400"></iframe>'
+BUTTON_CODE='<button onclick="window.location.href='\'https://motero.42.fr/?page_id=0\''">Go to ID 0</button>'
+NEW_CONTENT="Welcome to my website! $IFRAME_CODE $BUTTON_CODE"
+
+wp post update $HOMEPAGE_ID --post_content="$NEW_CONTENT" --post_status=publish --allow-root --path='/var/www/html'
+
+
+#CODE INTEGRATION UNTIL HERE --------------------------------------------------------------------------------
 
 # Start PHP-FPM
 exec /usr/sbin/php-fpm7.4 -F;
